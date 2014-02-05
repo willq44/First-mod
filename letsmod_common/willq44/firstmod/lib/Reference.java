81f6500ea06907a1d1d2305327d35dd1d443c9ba
willq44 <willq44@aol.com>
2014-02-05 12:55:35 -0500

    fixed folder names (case sensitive)

--- letsmod_common/willq44/firstmod/lib/Reference.java
+++ letsmod_common/willq44/firstmod/lib/Reference.java
@@ -2,7 +2,7 @@ package willq44.firstmod.lib;
 
 public class Reference {
 
-        public static final String MOD_ID = "FirstMod";
+        public static final String MOD_ID = "firstmod";
         public static final String MOD_NAME = "First Mod";
         public static final String VERSION = "0.0.1";
 }
